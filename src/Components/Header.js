8799cac7ebc5ea5e767a39e134ee5f0f906ecf04
jareiner90 <johnreiner1990@gmail.com>
2020-07-28 18:10:01 -0400

    set up basic data structure and created prop flow

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
+
+    if(props.headerData) {
+        var name = props.headerData.name
+    }
+
     return (
+
+        
+
         <div>
+            {console.log(name)}
             Header
         </div>
     )
